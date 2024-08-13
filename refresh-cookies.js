087daeebde6c8b23c069743f6439ca868187063f
Miriam Ukashi <shmmch@gmail.com>
2024-08-13 10:06:46 +0300

    Update refresh-cookies.js

--- refresh-cookies.js
+++ refresh-cookies.js
@@ -11,14 +11,18 @@ const fs = require('fs');
     // Extract cookies after logging in
     let cookies = await page.cookies();
 
-    // Filter out cookies with an expires value of -1
+    // Filter out cookies with an expires value of -1 (optional based on your needs)
     cookies = cookies.filter(cookie => cookie.expires !== -1);
+
+    // Print cookies to the terminal
+    console.log("Extracted Cookies:", cookies);
 
     // Convert to Netscape format
     const netscapeCookies = cookies.map(cookie => {
         return `${cookie.domain}\tTRUE\t${cookie.path}\t${cookie.secure ? 'TRUE' : 'FALSE'}\t${Math.floor(cookie.expires || 0)}\t${cookie.name}\t${cookie.value}`;
     }).join('\n');
 
+    // Save cookies to a file
     fs.writeFileSync('cookies.txt', netscapeCookies);
 
     await browser.close();
